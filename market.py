2118cd5988838a31859efa0302aa1ed50b6075ee
Tahmid-kabir-addin <tahmidkabiraddin@gmail.com>
2024-12-28 09:10:48 +0600

    templates directory added & tailwindcss configured

--- market.py
+++ market.py
@@ -1,18 +1,10 @@
 # turn debug mode on by export FLASK_DEBUG=1 to restart the server when we make changes
 # run the app by flask run
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/") # decorator: a function that wraps another function and adds some additional functionality
 def hello_world():
-    return "<h2>Hello, World!</h2>"
-
-@app.route("/about")
-def about():
-    return "<h2>About</h2>"
-
-@app.route("/profile/<username>")
-def profile(username):
-    return f"<h2>Profile for {username}</h2>"
+    return render_template("home.html")
